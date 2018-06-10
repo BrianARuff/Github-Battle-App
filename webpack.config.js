ec39421fe112502a23eb9c9f0633a60f6ebbbaa4
Brian Ruff <brff19@icloud.com>
2018-06-09 21:49:32 -0400

    api.js refactor to es6 and babel polyfill added for Promise.all

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
-const config = {
-  entry: ['babel-polyfill', './app/index.js'],
+var config = {
+    entry: ['babel-polyfill', './app/index.js'],
     output: {
-      path: path.resolve(__dirname, 'dist'),
-        filename: "index_bundle.js",
-        publicPath: "/",
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js',
+        publicPath: '/'
     },
     module: {
         rules: [
-            {test: /\.(js)$/, use: 'babel-loader'},
-            {test: /\.css$/, use: ['style-loader', 'css-loader']},
+            { test: /\.(js)$/, use: 'babel-loader' },
+            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
         ]
     },
     devServer: {
-      historyApiFallback: true,
+        historyApiFallback: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'app/index.html',
+            template: 'app/index.html'
         })
-    ],
-    mode: 'development',
+    ]
 };
 
-if (process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
     config.plugins.push(
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
             }
-        })
+        }),
+        new webpack.optimize.UglifyJsPlugin()
     )
 }
 
